14c4c2463051c6e09594573540b7a41ea995b742
Work_desktop <creator83@yandex.ru>
2017-04-19 14:00:14 +0300

    kj

--- cpp/inc/tftdriver.h
+++ cpp/inc/tftdriver.h
@@ -11,6 +11,7 @@ public:
 
 public:
 	virtual void drawPic (uint16_t x,uint16_t y, const uint16_t * arr,uint16_t length,uint16_t width) = 0;
+	virtual void symbol (uint16_t x, uint16_t y, const uint16_t color, const uint16_t fon, const uint8_t ch, Font & s)= 0;
 	//virtual void horLine (uint16_t x, uint16_t y, const uint16_t color, uint16_t length, uint8_t thick) = 0;
 	virtual ~Tftdriver(){}
 };
